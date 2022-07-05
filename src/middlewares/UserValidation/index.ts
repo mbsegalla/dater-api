dd67fc1a8ce7d615aad277260a4f04d77428dc52
mbsegalla <mbsegalla@gmail.com>
2022-07-05 18:31:04 -0300

    Ajuste de regex para aceitar ponto como caracter especial

--- src/middlewares/UserValidation/index.ts
+++ src/middlewares/UserValidation/index.ts
@@ -8,20 +8,21 @@ class UserValidationMiddleware {
         "string.empty": "Email is required",
         "string.email": "Email is invalid",
       }),
-      password: joi.string().min(8).max(30).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'password').required().messages({
+      password: joi.string().min(8).max(30).regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/, 'password').required().messages({
         "string.empty": "Password is required",
         "string.min": "Password length must be at least 8 characters long",
         "string.max": "Password length must be at most 30 characters long",
         "string.pattern.name": "Password must contain at least one lowercase letter, one uppercase letter, one number and one special character",
       }),
       birthDate: joi.date().required().messages({
-        "date.empty": "Date of birth is required",
-        "date.format": "Date of birth must be in the format DD-MM-YYYY",
-        "date.base": "Date of birth must be a valid date",
+        "date.empty": "Birth date is required",
+        "date.format": "Birth date must be in the format DD-MM-YYYY",
+        "date.base": "Birth date must be a valid date",
       }),
     });
     
     const { error } = userValidate.validate(req.body);
+
     if (error) {
       return res.status(422).json({ message: error.details[0].message });
     }
